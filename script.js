9878265ff3edcb104ccefa234ccfaaed0e4c6c1a
Katak Dalam Tempurung <humayungmirza@gmail.com>
2023-01-02 20:32:44 +0700

    new app

--- script.js
+++ script.js
@@ -1,123 +1,123 @@
-// ************************ Drag and drop ***************** //
-let dropArea;
-window.onload = function () {
-	// ************************ Drag and drop ***************** //
-	let dropArea = document.getElementById("drop-area")
+worker = new Worker("worker.js");
+worker.postMessage({ type: "start" });
+let modal;
+let closeButton;
+window.addEventListener("load", function () {
+  modal = document.querySelector(".modal");
+  closeButton = document.querySelector(".close-button");
 
-		// Prevent default drag behaviors
-		;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-			dropArea.addEventListener(eventName, preventDefaults, false)
-			document.body.addEventListener(eventName, preventDefaults, false)
-		})
+  closeButton.addEventListener("click", toggleLogModal);
+  window.addEventListener("click", windowOnClick);
+});
+function toggleLogModal(log) {
+  modal.classList.toggle("show-modal");
+}
 
-		// Highlight drop area when item is dragged over it
-		;['dragenter', 'dragover'].forEach(eventName => {
-			dropArea.addEventListener(eventName, highlight, false)
-		})
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleLogModal();
+  }
+}
 
-		;['dragleave', 'drop'].forEach(eventName => {
-			dropArea.addEventListener(eventName, unhighlight, false)
-		})
-
-	// Handle dropped files
-	dropArea.addEventListener('drop', handleDrop, false)
-
-	function preventDefaults(e) {
-		e.preventDefault()
-		e.stopPropagation()
-	}
-
-	function highlight(e) {
-		dropArea.classList.add('highlight')
-	}
-
-	function unhighlight(e) {
-		dropArea.classList.remove('active')
-	}
-
-
+const addTarget = () => {
+  // get value of textbox named username
+  const target = document.getElementById("username").value;
+  worker.postMessage({ type: "add", target: target });
 };
 
-function handleDrop(e) {
-	var dt = e.dataTransfer;
-	var files = dt.files;
-	processImage(files[0]);
-}
-
-function handleFiles(files) {
-	processImage(files[0]);
-}
-
-let loadedImage;
-let colorDegree = 1;
-let bw = false;
-function processImage(file) {
-	if (file.type.includes("image")) {
-		const image = URL.createObjectURL(file);
-		const output = document.getElementById('output')
-		output.src = image
-		loadedImage = file
-		const selectImage = document.getElementById('selectImage')
-		const ditherImage = document.getElementById('ditherOutput')
-		selectImage.style.display = 'none'
-		ditherImage.style.display = 'flex'
-		return
-	}
-	console.log('Not an image!')
-}
-
-var worker;
-let canvas = document.createElement("canvas");
-let ctx = canvas.getContext("2d");
-
-function prepareDither() {
-	const output = document.getElementById('output')
-	var img = new Image;
-
-	worker = new Worker('dither-worker.js')
-	canvas = document.createElement("canvas");
-	ctx = canvas.getContext("2d");
-	worker.onmessage = receivedWorkerMessage
-	img.onload = async function () {
-		canvas.width = img.width
-		canvas.height = img.height
-		ctx.drawImage(img, 0, 0);
-		const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-
-		console.log('sending to worker')
-		loading.style.display = 'flex'
-		worker.postMessage({
-			imageData: imageData,
-			colorDegree: colorDegree
-		})
-
-	}
-	img.src = URL.createObjectURL(loadedImage);
-}
-
-function receivedWorkerMessage(event) {
-	if (event.data.finished) {
-		ctx.putImageData(event.data.imageData, 0, 0)
-		const out = canvas.toDataURL("image/jpeg");
-		output.src = out
-		output.setAttribute('src', out)
-		loading.style.display = 'none'
-		progressValue.style.width = '0%'
-	} else {
-		const progress = event.data.progress
-		const maxProgress = event.data.maxProgress
-		const percent = `${Math.round(progress * 100/maxProgress)}%`
-		const progressValue = document.getElementById('progressValue')
-		progressValue.style.width = percent
-	}
-}
-
-function setColorDegree(event) {
-	colorDegree = event.target.value
-}
-
-async function downloadCanvas(el) {
-	const imageURI = canvas.toDataURL("image/jpg");
-	el.href = imageURI;
+const deleteTarget = (target) => {
+  worker.postMessage({ type: "delete", target: target });
 };
 
+const toggleTarget = (target) => {
+  worker.postMessage({ type: "toggle", target: target });
+};
+
+const viewLog = (target) => {
+  worker.postMessage({ type: "messageLog", target: target });
+};
+
+const createRow = (index, target, count) => {
+  return `<tr>
+            <td>${index}</td>
+            <td>${target.username}</td>
+            <td style="text-align: center">${count}</td>
+            <td style="text-align: center">
+              <span
+                class="textButton"
+                onclick="toggleTarget('${target.username}')"
+                style="color: ${target.active ? "green" : "red"}"
+                >${target.active ? "ON" : "OFF"}</span>
+
+              <span style="margin: 10px"></span>
+              <span class="textButton" href="#popup1" onclick="viewLog('${
+                target.username
+              }')">LOG</span>
+              <span style="margin: 10px"></span>
+              <span class="textButton" onclick="deleteTarget('${
+                target.username
+              }')">DEL</span>
+            </td>
+          </tr>`;
+};
+
+const emptyRow = () => {
+  // create a row with a single cell that spans all columns
+  return `<tr><td colspan="4" style="text-align: center">No Victims Yet</td></tr>`;
+};
+
+const updateView = (log, targets) => {
+  const table = document.getElementById("log");
+  table.innerHTML = "";
+  // if no targets, or no active targets, then return empty row
+  if (!targets) {
+    table.innerHTML = emptyRow();
+    return;
+  }
+  // sort targets based on active and count
+  targets.sort((a, b) => {
+    if (a.active && !b.active) return -1;
+    if (!a.active && b.active) return 1;
+    if (a.active && b.active) {
+      const countA = log[a.username] || 0;
+      const countB = log[b.username] || 0;
+      return countB - countA;
+    }
+    return 0;
+  });
+  
+  targets.forEach((target, index) => {
+    const count = log[target.username] || 0;
+    table.innerHTML += createRow(index + 1, target, count);
+  });
+};
+
+const prepareLogModal = (log) => {
+  if (log.length == 0) return;
+  const logViewer = document.getElementById("logViewer");
+  logViewer.innerHTML = "";
+  
+  log.forEach((message) => {
+    const li = document.createElement("li");
+    li.innerHTML = `Sent (${message.dateTime}): ` + message.text;
+    logViewer.appendChild(li);
+  });
+  const logTitle = document.getElementById("logTitle");
+  logTitle.innerHTML = `Log for ${log[0].username}`;
+};
+
+const receivedWorkerMessage = async (e) => {
+  switch (e.data.type) {
+    case "log":
+      updateView(e.data.log, e.data.targets);
+      break;
+    case "messageLog":
+      prepareLogModal(e.data.log);
+      toggleLogModal();
+      break;
+    default:
+      break;
+  }
+};
+
+worker.onmessage = receivedWorkerMessage;
